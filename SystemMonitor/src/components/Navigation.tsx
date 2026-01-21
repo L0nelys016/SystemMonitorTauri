522fb8d38aaaaf8113a46770a5afcc7189dc6bfc
Artem Volkov <artemka.volkov2017@yandex.ru>
2026-01-21 08:42:17 +0300

    add navigation

---
+++ SystemMonitor/src/components/Navigation.tsx
@@ -0,0 +1,19 @@
+import "./Navigation.css";
+
+export default function Navigation() {
+  return (
+    <nav class="navigation">
+      <ul>
+        <li>
+          <a href="#/">Главная</a>
+        </li>
+        <li>
+          <a href="#/about">О программе</a>
+        </li>
+        <li>
+          <a href="#/settings">Параметры</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
---
+++ SystemMonitor/src/pages/Main.tsx
@@ -0,0 +1,10 @@
+import "./Pages.css";
+
+export default function Main() {
+  return (
+    <div class="page">
+      <h1>🎯 System Monitor</h1>
+      <p>Добро пожаловать в приложение для мониторинга системы</p>
+    </div>
+  );
+}
